8eb1033f368d8f18e0e91558f6909bc3170ff6ec
Joseph Caburnay <caburnay.joseph@gmail.com>
2023-04-29 18:49:08 +0200

    feat: add base branch

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { OdooDevBranches } from './odoo_dev_branch';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "odoo-dev-plugin" is now active!'
-  );
-
   const rootPath =
     vscode.workspace.workspaceFolders &&
     vscode.workspace.workspaceFolders.length > 0
       ? vscode.workspace.workspaceFolders[0].uri.fsPath
       : undefined;
+
   vscode.window.registerTreeDataProvider(
     'odoo-dev-branches',
     new OdooDevBranches(rootPath)
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'odoo-dev-plugin.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from Odoo Dev!');
+    'odoo-dev-plugin.addBaseBranch',
+    async () => {
+      const input = await vscode.window.showInputBox({
+        placeHolder: 'e.g. 16.0',
+        prompt: 'Branch from odoo repo you want to become as base?',
+      });
+      // IMPROVEMENT: Validate if the input is proper branch from the odoo repo.
+      const keys = context.globalState.keys();
+      if (input) {
+        if (!keys.includes(input)) {
+          context.globalState.update(input, [input]);
+        } else {
+          vscode.window.showInformationMessage(
+            `'${input}' is already included as base branch.`
+          );
+        }
+      } else {
+        vscode.window.showErrorMessage('Please provide an input.');
+      }
     }
   );
 
